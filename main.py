266ce1d70460b9be5a9c78f6c30f32aea8b01968
Abhijit Dey <87671060+Avichiii@users.noreply.github.com>
2022-12-21 01:11:45 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,56 @@
+import random
+
+#main game function
+
+def gameWin(comp, you):
+
+    #if both player and comp chose same values then game is tie
+    
+    if comp == you:
+        return None
+
+    elif comp == 's':
+        if you == 'w':
+            return False
+        elif you == 'g':
+            return True
+
+    elif comp == 'w':
+        if you == 'g':
+            return False
+        elif you == 's':
+            return True
+
+    elif comp == 'g':
+        if you == 's':
+            return False
+        elif you == 'w':
+            return True
+
+#this is the func where computer chose random values
+
+print("Computer's turn: Snake(s) Water(w) or Gun(g)")
+randNO = random.randint(1,3)
+
+if randNO == 1:
+    comp = 's'
+elif randNO == 2:
+    comp = 'w'
+elif randNO == 3:
+    comp = 'g'
+
+you = input("Your Turn: Snake(s) Water(w) or Gun(g)")
+
+
+
+c = gameWin(comp, you)
+
+print(f"Comoputer chose {comp}")
+print(f"You chose {you}")
+
+if c == None:
+    print("This game is a tie") 
+elif c:
+    print("You win") 
+else:
+    print("You lose") 
